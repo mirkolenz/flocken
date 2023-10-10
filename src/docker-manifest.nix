857843b894515f852f0f12663bce40be9d19500f
Mirko Lenz <mirko@mirkolenz.com>
2023-10-10 10:31:56 +0200

    refactor: update variable names

--- src/docker-manifest.nix
+++ src/docker-manifest.nix
@@ -12,23 +12,24 @@
   targetProtocol ? "docker://",
   format ? "oci",
   extraTags ? [],
+  annotations ? {},
   ...
 }: let
-  cleanVersion = lib.removePrefix "v" version;
+  version = lib.removePrefix "v" version;
   manifestName = "flocken";
-  allNames = names ++ (lib.optional (name != "") name);
-  allTags =
+  names = names ++ (lib.optional (name != "") name);
+  tags =
     extraTags
     ++ (lib.optional (branch != "") branch)
     ++ (lib.optional latest "latest")
-    ++ (lib.optional (cleanVersion != "") cleanVersion)
-    ++ (lib.optionals (cleanVersion != "" && !lib.hasInfix "-" cleanVersion) [
-      (lib.versions.majorMinor cleanVersion)
-      (lib.versions.major cleanVersion)
+    ++ (lib.optional (version != "") version)
+    ++ (lib.optionals (version != "" && !lib.hasInfix "-" version) [
+      (lib.versions.majorMinor version)
+      (lib.versions.major version)
     ]);
 in
-  assert (lib.assertMsg (builtins.length allNames > 0) "At least one name must be specified");
-  assert (lib.assertMsg (builtins.length allTags > 0) "At least one tag must be specified");
+  assert (lib.assertMsg (builtins.length names > 0) "At least one name must be specified");
+  assert (lib.assertMsg (builtins.length tags > 0) "At least one tag must be specified");
     writeShellScriptBin "docker-manifest" ''
       set -x # echo on
       if ${lib.getExe buildah} manifest exists "${manifestName}"; then
@@ -38,8 +39,8 @@ in
       for IMAGE in ${builtins.toString images}; do
         ${lib.getExe buildah} manifest add "${manifestName}" "${sourceProtocol}$IMAGE"
       done
-      for NAME in ${builtins.toString allNames}; do
-        for TAG in ${builtins.toString allTags}; do
+      for NAME in ${builtins.toString names}; do
+        for TAG in ${builtins.toString tags}; do
           ${lib.getExe buildah} manifest push --all --format ${format} "${manifestName}" "${targetProtocol}$NAME:$TAG"
         done
       done
