0d9d01b0d82a575482ead1141291f916755ebeeb
Mirko Lenz <mirko@mirkolenz.com>
2023-10-11 11:42:20 +0200

    feat(manifest) allow specifying annotations

--- src/docker-manifest.nix
+++ src/docker-manifest.nix
@@ -27,6 +27,22 @@
       (lib.versions.majorMinor cleanVersion)
       (lib.versions.major cleanVersion)
     ]);
+
+  getLeaves = attrset: path:
+    if builtins.isAttrs attrset
+    then
+      builtins.concatLists (
+        lib.mapAttrsToList
+        (key: value: getLeaves value (path ++ [key]))
+        attrset
+      )
+    else [
+      {
+        name = builtins.concatStringsSep "." path;
+        value = attrset;
+      }
+    ];
+  parsedAnnotations = builtins.listToAttrs (getLeaves annotations []);
 in
   assert (lib.assertMsg (builtins.length allNames > 0) "At least one name must be specified");
   assert (lib.assertMsg (builtins.length allTags > 0) "At least one tag must be specified");
@@ -35,9 +51,20 @@ in
       if ${lib.getExe buildah} manifest exists "${manifestName}"; then
         ${lib.getExe buildah} manifest rm "${manifestName}"
       fi
-      ${lib.getExe buildah} manifest create "${manifestName}"
-      for IMAGE in ${builtins.toString images}; do
-        ${lib.getExe buildah} manifest add "${manifestName}" "${sourceProtocol}$IMAGE"
+      manifest=$(${lib.getExe buildah} manifest create "${manifestName}")
+      for image in ${builtins.toString images}; do
+        manifestOutput=$(${lib.getExe buildah} manifest add "$manifest" "${sourceProtocol}$image")
+        ${
+        if builtins.length (builtins.attrNames parsedAnnotations) > 0
+        then ''
+          manifestSplit=($manifestOutput)
+          digest=''${manifestSplit[1]}
+          ${lib.getExe buildah} manifest annotate \
+            ${builtins.toString (lib.mapAttrsToList (key: value: "--annotation \"${key}=${value}\"") parsedAnnotations)} \
+            "$manifest" "$digest"
+        ''
+        else ""
+      }
       done
       for name in ${builtins.toString allNames}; do
         for tag in ${builtins.toString allTags}; do
