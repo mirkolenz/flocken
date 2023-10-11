39d96e6d179b50e76f74dcc6dfc15ee4dde59dd8
Mirko Lenz <mirko@mirkolenz.com>
2023-10-11 11:41:41 +0200

    fix: use correct variable naming again

--- src/docker-manifest.nix
+++ src/docker-manifest.nix
@@ -15,21 +15,21 @@
   annotations ? {},
   ...
 }: let
-  version = lib.removePrefix "v" version;
+  cleanVersion = lib.removePrefix "v" version;
   manifestName = "flocken";
-  names = names ++ (lib.optional (name != "") name);
-  tags =
+  allNames = names ++ (lib.optional (name != "") name);
+  allTags =
     extraTags
     ++ (lib.optional (branch != "") branch)
     ++ (lib.optional latest "latest")
-    ++ (lib.optional (version != "") version)
-    ++ (lib.optionals (version != "" && !lib.hasInfix "-" version) [
-      (lib.versions.majorMinor version)
-      (lib.versions.major version)
+    ++ (lib.optional (cleanVersion != "") cleanVersion)
+    ++ (lib.optionals (cleanVersion != "" && !lib.hasInfix "-" cleanVersion) [
+      (lib.versions.majorMinor cleanVersion)
+      (lib.versions.major cleanVersion)
     ]);
 in
-  assert (lib.assertMsg (builtins.length names > 0) "At least one name must be specified");
-  assert (lib.assertMsg (builtins.length tags > 0) "At least one tag must be specified");
+  assert (lib.assertMsg (builtins.length allNames > 0) "At least one name must be specified");
+  assert (lib.assertMsg (builtins.length allTags > 0) "At least one tag must be specified");
     writeShellScriptBin "docker-manifest" ''
       set -x # echo on
       if ${lib.getExe buildah} manifest exists "${manifestName}"; then
@@ -39,9 +39,9 @@ in
       for IMAGE in ${builtins.toString images}; do
         ${lib.getExe buildah} manifest add "${manifestName}" "${sourceProtocol}$IMAGE"
       done
-      for NAME in ${builtins.toString names}; do
-        for TAG in ${builtins.toString tags}; do
-          ${lib.getExe buildah} manifest push --all --format ${format} "${manifestName}" "${targetProtocol}$NAME:$TAG"
+      for name in ${builtins.toString allNames}; do
+        for tag in ${builtins.toString allTags}; do
+          ${lib.getExe buildah} manifest push --all --format ${format} "$manifest" "${targetProtocol}$name:$tag"
         done
       done
     ''
